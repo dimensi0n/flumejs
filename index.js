a8052b86563a249fd50560fca9eaf64847d60c93
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-06-02 16:11:17 +0200

    Db functions added

--- index.js
+++ index.js
@@ -77,3 +77,53 @@ async function AllUsers() {
     let res = await fetch('http://localhost:8080/auth/all', init)
     return res
 }
+
+async function NewDb(name, content) {
+    let init = {
+        method: 'POST',
+        headers: {
+            token: token
+        },
+        body: JSON.stringify({
+            name: name,
+            content: content
+        })
+    }
+    let res = await fetch('http://localhost:8080/db/create', init)
+    return res
+}
+
+async function GetDb(name) {
+    let init = {
+        method: 'GET',
+        headers: {
+            token: token
+        }
+    }
+    let res = await fetch('http://localhost:8080/db/read/'+name, init)
+    return res
+}
+
+async function UpdateDb(name, content) {
+    let init = {
+        method: 'PUT',
+        headers: {
+            token: token
+        },
+        body: JSON.stringify({
+            name: name,
+            content: content
+        })
+    }
+    let res = await fetch('http://localhost:8080/db/write', init)
+    return res
+}
+
+UpdateDb('thedb', JSON.stringify(["new content"]))
+    .then(res => res.text())
+    .then(body => console.log(body))
+
+GetDb('thedb')
+    .then(res => res.text())
+    .then(body => console.log(body))
+
